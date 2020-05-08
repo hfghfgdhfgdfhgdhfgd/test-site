c5fa7d3304de85afaa005eb8b84d51c478451439
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-08 18:25:08 +0800

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,25 @@ export default () => (
   <Layout>
     start page
     contacts form
-    <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-      <input type="hidden" name="bot-field" />
-    </form> 
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
 );
