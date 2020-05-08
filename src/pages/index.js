ccff73ae0bd39c286193e8b7e62f3a3cf1eff02a
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-08 19:18:24 +0800

    add form

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,21 +5,9 @@ export default () => (
   <Layout>
     start page
     contacts form
-    <form name="contact1" method="POST" data-netlify="true">
+    <form name="formWork" method="POST" data-netlify="true">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
       </p>
       <p>
         <button type="submit">Send</button>
