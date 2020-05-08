a1fea30ef35f6a400f5e880e4ab1bbba3ab74756
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-08 18:19:26 +0800

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,9 @@ import Layout from "../components/layout";
 export default () => (
   <Layout>
     start page
+    contacts form
+    <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="bot-field" />
+    </form> 
   </Layout>
 );
