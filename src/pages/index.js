2081c0f3f8486615663f30c845c912a8cd4abd87
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-05-08 19:51:00 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
 
-export default () => (
-  <Layout>
+export default () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = e => {
+    
+    const encode = (data) => {
+      return Object.keys(data)
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+          .join("&");
+    }
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "workForm1", name: name})
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+    e.preventDefault();
+  };
+  return <Layout>
     start page
     contacts form
-    <form name="workForm1" method="POST" class="contact__form" netlify-honeypot="bot-field" data-netlify="true">
+    <form onSubmit={handleSubmit} name="workForm1" method="POST" class="contact__form" netlify-honeypot="bot-field" data-netlify="true">
       <p>
-        <label>Your Name: <input type="text" name="name" /></label>   
+        <label>Your Name: 
+          <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+        </label>   
       </p>
-      <input type="hidden" name="form-name" value="workForm1" />
     </form>
   </Layout>
-);
+}
